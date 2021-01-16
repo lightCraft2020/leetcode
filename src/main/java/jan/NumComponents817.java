ac5405c29bf949798056e5e3808489db866f10a3
liuxc <gdutliuch@163.com>
2021-01-17 00:20:50 +0800

    day 16

--- src/main/java/jan/NumComponents817.java
+++ src/main/java/jan/NumComponents817.java
@@ -1,5 +1,8 @@
 package jan;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created By think
  * Created At 2021/1/16 23:52
@@ -13,9 +16,29 @@ public class NumComponents817 {
     }
 
     public int numComponents(ListNode head, int[] G) {
-
-
-        return 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < G.length; i++) {
+            set.add(G[i]);
+        }
+        if(G.length == 0){
+            return 0;
+        }
+        int count = 0;
+        boolean stop = false;
+        while(head != null){
+            if(set.contains(head.val)){
+                stop = true;
+            } else {
+                if(stop){
+                    count++;
+                }
+                stop=false;
+            }
+            head = head.next;
+        }
+        if(stop)
+            count++;
+        return count;
     }
 
 
