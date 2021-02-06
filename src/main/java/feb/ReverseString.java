97461d2f63ca6ee7f9a70bc598535a8ed0e5c959
liuxc <gdutliuch@163.com>
2021-02-06 23:45:06 +0800

    day 35

---
+++ src/main/java/feb/ReverseString.java
@@ -0,0 +1,24 @@
+package feb;
+
+/**
+ * Created By think
+ * Created At 2021/2/6 23:40
+ */
+public class ReverseString {
+
+    public static void reverseString(char[] s) {
+        if(s.length == 0){
+            return;
+        }
+        int n = s.length / 2;
+        for(int i=0; i<n; i++){
+            char t = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = t;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
