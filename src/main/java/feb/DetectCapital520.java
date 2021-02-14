488b4d50e4bbd3343d1a9a25525d25e4c8ae9156
liuxc <gdutliuch@163.com>
2021-02-15 00:09:38 +0800

    day 39

--- src/main/java/feb/DetectCapital520.java
+++ src/main/java/feb/DetectCapital520.java
@@ -6,21 +6,54 @@ package feb;
  */
 public class DetectCapital520 {
 
-    public boolean detectCapitalUse(String word) {
+    public static boolean detectCapitalUse(String word) {
         boolean ret = false;
-
-
-
-
+        if(isAllCapital(word) || isAllLower(word) || isFirstCapital(word)){
+            return true;
+        }
         return ret;
     }
 
-    private boolean isAllCapital(String word){
-        boolean ret = false;
-
-
-
-
+    private static boolean isAllCapital(String word){
+        boolean ret = true;
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < 'A' || arr[i] > 'Z'){
+                ret = false;
+                break;
+            }
+        }
         return ret;
+    }
+    private static boolean isAllLower(String word){
+        boolean ret = true;
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < 'a' || arr[i] > 'z'){
+                ret = false;
+                break;
+            }
+        }
+        return ret;
+    }
+
+    private static boolean isFirstCapital(String word){
+        boolean ret = true;
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if(i == 0 && (arr[i] < 'A' || arr[i] > 'Z')){
+                ret = false;
+                break;
+            }
+            if((i > 0) && (arr[i] < 'a' || arr[i] > 'z')){
+                ret = false;
+                break;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(detectCapitalUse("Leetcode"));
     }
 }
