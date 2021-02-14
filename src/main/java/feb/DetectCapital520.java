97223a4e3b062c2cbc606952c8be4c189f1aa010
liuxc <gdutliuch@163.com>
2021-02-14 23:56:53 +0800

    day 39

---
+++ src/main/java/feb/DetectCapital520.java
@@ -0,0 +1,26 @@
+package feb;
+
+/**
+ * Created By think
+ * Created At 2021/2/14 23:52
+ */
+public class DetectCapital520 {
+
+    public boolean detectCapitalUse(String word) {
+        boolean ret = false;
+
+
+
+
+        return ret;
+    }
+
+    private boolean isAllCapital(String word){
+        boolean ret = false;
+
+
+
+
+        return ret;
+    }
+}
