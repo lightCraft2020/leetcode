2798c021a01412ab89916c4e22a23a34c17615b4
liuxc <gdutliuch@163.com>
2021-02-20 23:59:22 +0800

    day 45

---
+++ src/main/java/feb/KeyboardRow500.java
@@ -0,0 +1,30 @@
+package feb;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created By think
+ * Created At 2021/2/20 23:56
+ */
+public class KeyboardRow500 {
+
+
+    public String[] findWords(String[] words) {
+
+
+
+        return null;
+    }
+
+    private Set<Character> getSet(String strs){
+        Set<Character> set = new HashSet<>();
+        for(char item : strs.toCharArray()){
+           // set.add(Arrays.asList());
+        }
+
+
+        return set;
+    }
+}
