b67622473f8341541cb6a374de961dd2231cec51
liuxc <gdutliuch@163.com>
2021-02-21 00:07:16 +0800

    day 45

--- src/main/java/feb/KeyboardRow500.java
+++ src/main/java/feb/KeyboardRow500.java
@@ -1,8 +1,6 @@
 package feb;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created By think
@@ -13,18 +11,35 @@ public class KeyboardRow500 {
 
     public String[] findWords(String[] words) {
 
+        Set<Character> setOne = getSet("qwertyuiop");
+        Set<Character> setTwo = getSet("asdfghjkl");
+        Set<Character> setThree = getSet("zxcvbnm");
 
+        List<String> list = new ArrayList<>();
 
-        return null;
+        for(String word : words){
+            if(contains(setOne, word) ||
+                    contains(setTwo, word) ||contains(setThree, word)){
+                list.add(word);
+            }
+        }
+        return list.toArray(new String[list.size()]);
+    }
+
+    private boolean contains(Set<Character> set, String word){
+        for(char c : word.toLowerCase().toCharArray()){
+            if(!set.contains(c)){
+                return false;
+            }
+        }
+        return true;
     }
 
     private Set<Character> getSet(String strs){
         Set<Character> set = new HashSet<>();
         for(char item : strs.toCharArray()){
-           // set.add(Arrays.asList());
+            set.add(item);
         }
-
-
         return set;
     }
 }
