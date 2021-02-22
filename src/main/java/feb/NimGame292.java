0cd269c89e17aa002dffa96eac5e8bf1de5a05c7
liuxc <gdutliuch@163.com>
2021-02-22 22:26:03 +0800

    day 47

---
+++ src/main/java/feb/NimGame292.java
@@ -0,0 +1,16 @@
+package feb;
+
+/**
+ * Created By think
+ * Created At 2021/2/22 22:18
+ */
+public class NimGame292 {
+
+
+    public boolean canWinNim(int n) {
+        return n % 4 !=0;
+    }
+
+
+
+}
